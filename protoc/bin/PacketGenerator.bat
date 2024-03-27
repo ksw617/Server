a1d5094fa6ca03390a7df936a25a3047382c4323
ksw617 <ksw617@gmail.com>
2024-03-27 20:13:18 +0900

    update

--- protoc/bin/PacketGenerator.bat
+++ protoc/bin/PacketGenerator.bat
@@ -1,2 +1,10 @@
+pushed %~dp0
+
 protoc.exe --proto_path=./ --cpp_out=./ ./Protocol.proto
 IF ERRORLEVEL 1 PAUSE
+
+XCOPY /Y Protocol.pb.cc "../../Server"
+XCOPY /Y Protocol.pb.h "../../Server"
+
+XCOPY /Y Protocol.pb.cc "../../Client"
+XCOPY /Y Protocol.pb.h "../../Client"
